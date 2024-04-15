4eaeb2e09f004a70f8d90e42ffebc75debc42b22
TinaMcComiskey <tinamccomiskey@gmail.comgit>
2024-04-14 21:41:43 -0700

    Implemente QuestionID, main

--- chapt_two_workthrough/src/main.rs
+++ chapt_two_workthrough/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr; 
+
 struct Question {
     id: QuestionId,
     title: String,
@@ -5,24 +8,40 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-struct QuestionId(String);
-
- impl Question {
-    fn new(
- id: QuestionId, 
-title: String, 
-content: String, 
-tags: Option<Vec<String>>
-     ) -> Self {
- Question {
-    id,
-    title,
-    content,
-    tags,
- }
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "No id provided")
+            ),
+        }
     }
- }
+}
+
+impl Question {
+    fn new(
+        id: QuestionId, 
+        title: String, 
+        content: String, 
+        tags: Option<Vec<String>>
+      ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
 
 fn main() {
-    
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(), 
+        "Content of question".to_string(), 
+        Some(vec!("faq".to_string())),
+    );
+    println!("{:?}", question);
 }
