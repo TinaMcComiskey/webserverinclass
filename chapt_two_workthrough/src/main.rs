4ef16dca0185c19794ce8eda9528062948dc64d7
TinaMcComiskey <tinamccomiskey@gmail.comgit>
2024-04-22 23:46:50 -0700

    Added CORS responses

--- chapt_two_workthrough/src/main.rs
+++ chapt_two_workthrough/src/main.rs
@@ -1,7 +1,8 @@
 use std::io::{Error, ErrorKind};
 use std::str::FromStr; 
 use serde::Serialize;
-use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
+use warp::{Filter, http::Method, filters::cors::CorsForbidden, 
+            reject::Reject, Rejection, Reply, http::StatusCode};
 
 #[derive(Debug, Serialize)]
 struct QuestionId(String);
@@ -65,14 +66,20 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(_invalid_id) = r.find::<InvalidId>() {
+    println!("{:?}", r);
+    if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid ID presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
@@ -80,12 +87,18 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
  #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("not-in-the-request")
+        .allow_methods(
+            &[Method::PUT, Method::DELETE, Method::GET, Method::POST]
+        );
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
-    let routes = get_items;
+    let routes = get_items.with(cors);
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
